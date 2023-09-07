137ad317d8e65670f2268dc2609ae1a1e10c2d0c
Manuel Alejandro Moscoso Domínguez <manuel@mobiquos.cl>
2023-09-06 22:22:56 -0300

    First Commit

---
+++ app/src/main/java/cl/mmoscoso/reactions/MainActivity.kt
@@ -0,0 +1,36 @@
+package cl.mmoscoso.reactions
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intentPreferences = Intent(this, SettingsActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            R.id.action_about -> {
+                val intentPreferences = Intent(this, AboutActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+}
---
+++ app/src/main/java/cl/mmoscoso/reactions/SplashActivity.kt
@@ -0,0 +1,58 @@
+package cl.mmoscoso.reactions
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Load the fade-in animation
+        val fadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fadein)
+
+        // Load the growth animation
+        val scaleUp = AnimationUtils.loadAnimation(applicationContext, R.anim.scaleup)
+
+        // Create an AnimationSet to combine both animations
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(fadeIn)
+        animationSet.addAnimation(scaleUp)
+
+
+        val imageView = findViewById<ImageView>(R.id.imageReactions)
+
+        fadeIn.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // Animation ended; proceed to MainActivity
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Optional: Close the splash screen activity
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // Animation repeated
+            }
+        })
+        imageView.startAnimation(fadeIn)
+        val delayMillis = 7000L // 2 seconds (adjust as needed)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the splash screen activity
+        }, delayMillis)
+    }
+}
