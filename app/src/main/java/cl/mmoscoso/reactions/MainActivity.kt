4930c240b407daafaf60dd8bf96a00abd9aad538
Manuel Alejandro Moscoso Domínguez <manuel@mobiquos.cl>
2023-09-14 09:21:48 -0300

    Actualización del proyecto considerando trabajar con una notificacion relacionada con el sonido. Esta notificacion opera durante la ejecucion de la app y se elimina al salir.|

--- app/src/main/java/cl/mmoscoso/reactions/MainActivity.kt
+++ app/src/main/java/cl/mmoscoso/reactions/MainActivity.kt
@@ -1,18 +1,50 @@
 package cl.mmoscoso.reactions
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var CHANNEL_ID = "MAINACTIVTY"
+    private var NOTIFICATION_ID = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Create a notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Background Audio",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Background Audio Playback")
+            .setContentText("Your app is playing audio in the background.")
+            .setSmallIcon(R.drawable.ic_launcher_background) // Replace with your notification icon
+            .setPriority(NotificationCompat.PRIORITY_LOW) // Adjust priority as needed
+            .build()
+
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.notify(NOTIFICATION_ID, notification)
 
 
         val buttonOKAbout = findViewById<Button>(R.id.aboutButton)
@@ -45,6 +77,33 @@ class MainActivity : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.cancel(NOTIFICATION_ID)
     }
 
 }
